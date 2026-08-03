7c00d289e087f1ecb797deb42abb28cf9d96e01e
Kitt <519840313@qq.com>
2026-08-04 00:34:22 +0800

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -6,9 +6,9 @@ CONF'
 sudo sysctl --system > /dev/null 2>&1
 STATUS=$(cat /proc/sys/net/ipv6/conf/all/disable_ipv6 2>/dev/null)
 if [ "$STATUS" = "1" ]; then
-    echo -e "\033[32m[成功] ！\033[0m"
+    echo -e "\033[32m[成功] IPv6禁用成功！\033[0m"
 else
-    echo -e "\033[31m[失败] 请检查系统权限。\033[0m"
+    echo -e "\033[31m[失败] IPv6禁用失败,请检查系统权限。\033[0m"
 fi
 EOF
 )"
